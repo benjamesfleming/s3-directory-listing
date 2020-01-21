b4b65ebf55c147f48fa829c95bb00addee8f976e
Ben Fleming <benfleming@localhost.localdomain>
2020-01-22 11:39:19 +1300

    add html-beautify

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const HtmlBeautifyPlugin = require('html-beautify-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'production',
     entry: './src/index.js',
     output: {
         path: __dirname + '/dist',
@@ -24,6 +26,14 @@ module.exports = {
         }),
         new MiniCssExtractPlugin(),
         new HtmlWebpackPlugin({ template: 'src/index.html', inlineSource: '.(js|css)$' }),
-        new HtmlWebpackInlineSourcePlugin()
+        new HtmlWebpackInlineSourcePlugin(),
+        new HtmlBeautifyPlugin({
+            config: {
+                html: {
+                    "end_with_newline ": false,
+                    unformatted: ['script']
+                }
+            }
+        })
     ]
 };
